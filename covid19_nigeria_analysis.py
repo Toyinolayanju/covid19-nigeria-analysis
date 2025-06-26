7bc890298e66606f2842e2baae5124c156ec5716
Olayanju Oluwatoyin Favour <toyinolayanju614@gmail.com>
2025-06-26 23:00:12 +0100

    Update covid19_nigeria_analysis.py

--- covid19_nigeria_analysis.py
+++ covid19_nigeria_analysis.py
@@ -1,11 +1,7 @@
-
-# COVID-19 Data Analysis for Nigeria
-# Author: Olayanju Oluwatoyin Favour
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load data
+# Load dataset
 url = 'https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv'
 df = pd.read_csv(url)
 
@@ -15,10 +11,10 @@ nigeria_df = df[df['Country'] == 'Nigeria']
 # Convert date column to datetime
 nigeria_df['Date'] = pd.to_datetime(nigeria_df['Date'])
 
-# Plot confirmed cases over time
-plt.figure(figsize=(10, 5))
+# Plotting the data
+plt.figure(figsize=(12, 6))
 plt.plot(nigeria_df['Date'], nigeria_df['Confirmed'], label='Confirmed Cases', color='blue')
-plt.plot(nigeria_df['Date'], nigeria_df['Recovered'], label='Recovered', color='green')
+plt.plot(nigeria_df['Date'], nigeria_df['Recovered'], label='Recovered Cases', color='green')
 plt.plot(nigeria_df['Date'], nigeria_df['Deaths'], label='Deaths', color='red')
 plt.title('COVID-19 Trends in Nigeria')
 plt.xlabel('Date')
@@ -26,7 +22,5 @@ plt.ylabel('Number of Cases')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-
-# Save the plot
-plt.savefig('nigeria_covid19_trends.png')
+plt.savefig('nigeria_covid19_trend.png')  # Saves the plot
 plt.show()
